6cfd96857673c884bbd46bdf279cff2fcb6464b0
Surbhi Jha @resolutesoul <jhasurbhi21@gmail.com>
2024-07-20 18:02:05 +0530

    add intro

--- 01_basics/Intro.js
+++ 01_basics/Intro.js
@@ -1,11 +1,23 @@
 console.log ("Surbhi")
 console.log ("Hello World")
-/* 1.JavaScript and Java are completely different languages, both in concept and design.
-2. Javascript was invented by Brendan Eich in 1995, and beccome an ECMA standard in 1997
-3. ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.
+
+
+/* 
+                    History & Evolution-
+# JavaScript and Java are completely different languages, both in concept and design.
+# Javascript was invented by "Brendan Eich" in 1995 [Mocha]
+  1997 - ECMAScript
+  2005 - AJAX [Asynhronous JS & XML]
+  2009 - Node.js was released
+  2015 - ECMAScript 6 [ES6] (modern features were added)
+  2016 to present- Continued Updates
 In HTML  the javascript code is inserted in <script></script> tag
 
-
-
+                    Defination-
+# Javascript is a programming language used to make web pages interactive. It can control the behaviour of different elements on a website such as - animations, forms, dynamic content updates.
+# It works alongside-
+  HTML (Structures the Content)[Skeleton]
+  CSS (Styles the Content) [Skin]
+  JavaScript (Provides the Interactivity) [Brain/Muscles]
 
 */
