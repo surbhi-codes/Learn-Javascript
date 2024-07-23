241e8b8cd922e407ade96fff852af11f672f1f55
surbhi-codes <jhasurbhi21@gmail.com>
2024-07-23 18:54:11 +0530

    add changes

--- 01_basics/Intro.js
+++ 01_basics/Intro.js
@@ -5,7 +5,7 @@ console.log ("Hello World")
 /* 
                     History & Evolution-
 # JavaScript and Java are completely different languages, both in concept and design.
-# Javascript was invented by "Brendan Eich" in 1995 [Mocha]
+# Javascript [Livescript] was invented by "Brendan Eich" in 1995 [Mocha]
   1997 - ECMAScript
   2005 - AJAX [Asynhronous JS & XML]
   2009 - Node.js was released
@@ -15,6 +15,8 @@ In HTML  the javascript code is inserted in <script></script> tag
 
                     Defination-
 # Javascript is a programming language used to make web pages interactive. It can control the behaviour of different elements on a website such as - animations, forms, dynamic content updates.
+# Javascript is a Synchronous [executes one command at a time] & single-threaded language.
+# JavaScript is both an imperative [how to do something with detailed instructions] and declarative [what you want to achieve & let the language figure out the details] type of language.
 # It works alongside-
   HTML (Structures the Content)[Skeleton]
   CSS (Styles the Content) [Skin]
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,9 +1,41 @@
-// Javascript Variables:- var, let, const
+// Javascript Variables:- are used to store and manage data
 // var - can be re-declared & updated. A global scope variable
 // let - cannot be re-declared but can be updated. A block scope variable
 // const- cannot be re-declared & updated. A block scope variable
 
 //  account_id/ account_Id/ accountId
+
+// var keyword- var= 10
+  var a = "Hello Surbhi"
+  var b = 3;
+  var c = 5;
+  var d = b + c;
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+
+// let keyword- 
+ let a = "Hello Surbhi"
+ let b = "learn";
+ let c = "21";
+ let d = b + c;
+
+ console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+
+// const keyword-
+const a = "Hello Surbhi"
+console.log(a);
+
+const b = 365;
+console.log(b);
+
+const c = "21";
+console.log(c);
 
 const studentId = 129090
 let studentName = "Surbhi"
