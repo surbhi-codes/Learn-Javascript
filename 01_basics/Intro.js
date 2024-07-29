2b939c8525b83759b96f948a25c0637fecb6f040
surbhi-codes <jhasurbhi21@gmail.com>
2024-07-29 13:32:54 +0530

    add changes

--- 01_basics/Intro.js
+++ 01_basics/Intro.js
@@ -3,23 +3,56 @@ console.log ("Hello World")
 
 
 /* 
-                    History & Evolution-
+                    -: HISTORY & EVOLUTION :-
 # JavaScript and Java are completely different languages, both in concept and design.
-# Javascript [Livescript] was invented by "Brendan Eich" in 1995 [Mocha]
+# Javascript [initially (Livescript)] was invented by "Brendan Eich" in 1995 [Mocha]
   1997 - ECMAScript
   2005 - AJAX [Asynhronous JS & XML]
   2009 - Node.js was released
   2015 - ECMAScript 6 [ES6] (modern features were added)
   2016 to present- Continued Updates
-In HTML  the javascript code is inserted in <script></script> tag
+# In HTML  the javascript code is inserted in <script></script> tag
+# A single <script> tag can’t have both the src attribute and code inside.
+# A script in an external file can be inserted with <script src="path/to/script.js"></script>
 
-                    Defination-
+                   -: DEFINATION :-
 # Javascript is a programming language used to make web pages interactive. It can control the behaviour of different elements on a website such as - animations, forms, dynamic content updates.
+# The programs in this language are called scripts
 # Javascript is a Synchronous [executes one command at a time] & single-threaded language.
 # JavaScript is both an imperative [how to do something with detailed instructions] and declarative [what you want to achieve & let the language figure out the details] type of language.
 # It works alongside-
   HTML (Structures the Content)[Skeleton]
   CSS (Styles the Content) [Skin]
-  JavaScript (Provides the Interactivity) [Brain/Muscles]
+  JavaScript (Provides the Interactivity)(make web pages alive) [Brain/Muscles]
+
+# JAVASCRIPT UNIQUENESS-
+1. Ful
+l integration with HTML/ CSS
+2. Simple things are done simply
+3. Supported by all major browser
+
+# There are many languages that get  'Transpiled' [converted] by JS before they run in browser
+Example-
+CoffeeScript:- Description: Syntactic sugar for JavaScript.
+               Features: Shorter syntax for clearer and more precise code.
+               Popular With: Ruby developers.
+
+TypeScript:- Description: Adds strict data typing to JavaScript.
+             Features: Simplifies the development and support of complex systems.
+             Developed By: Microsoft.
+
+Flow:- Description: Adds data typing to JavaScript in a different way than TypeScript.
+       Developed By: Facebook.
+
+Dart:- Description: A standalone language with its own engine.
+       Features: Runs in non-browser environments (like mobile apps) and can be transpiled to JavaScript.
+       Developed By: Google.
+
+Brython:- Description: A Python transpiler to JavaScript.
+          Features: Enables writing applications in pure Python without JavaScript.
+
+Kotlin:- Description: A modern, concise, and safe programming language.
+         Features: Can target the browser or Node.js environments.
+and many more.
 
 */
