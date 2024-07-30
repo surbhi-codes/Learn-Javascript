80049dc3087b744908117a2156be414cf3946434
surbhi-codes <jhasurbhi21@gmail.com>
2024-07-30 17:14:41 +0530

    add changes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -1,3 +1,6 @@
+/*                          -: DATATYPES :-
+  Data types specify the kind of data that can be stored and manipulated within a program     
+*/
 // 1.Primitive data types:- simple,non-modifiable, stored by value
     // number => 2 to power 53 ( Integers, floats)
     // bigint => large int
@@ -8,11 +11,6 @@
     // Symbol => A unique  & immutable value
     
 //  there are so-called “special numeric values” which also belong to this data type: Infinity, -Infinity and NaN.
-
-
-
-
-
 
 //     2.Non-primitive data types(Object References):- complex, modifiable, stored by reference
         //Objects (Collections of key-value pairs)
@@ -28,11 +26,41 @@
         2
       ); //  code readability should be high
 
-      console.log(Surbhi);
-      let name = "Surbhi"
-      let phoneNumber = 1234567890
-      let isLoggegIn = false
-      let state;
+      let num = 300; // Number
+      let price = 30.5;
+
+      let bigInt = 9076655445132436576n; // Bigint
+
+      let greeting = "Hello"; // String
+      let message = "Bye";
+
+      let inProgress = true; // Boolean
+      let completed = false;
+
+      let x = null;
+      console.log(null == undefined); // Null
+
+      let y;
+      console.log(y); // Undefined
+
+      const id = Symbol('123');
+      const anotherId = Symbol('123');
+
+      console.log(id === anotherId);
+
+      // Array, Ojects, Functions
+
+      const heros = ["spiderman", "ironman", "superman"];
+      let myobj = {
+        name: "surbhi",
+        age: "10",
+      }
+
+       const myFunction = function(){
+         console.log("Hello world");
+       }
+
+       console.log(typeof anotherId);
 
 
       // The typeof operator allows us to see which type is stored in a variable.
