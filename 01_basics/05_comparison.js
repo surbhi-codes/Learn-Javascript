ff1225539660f480d327c30c4781ac99bf18b9cd
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-05 08:38:43 +0530

    add comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// These creates confusion better try to avoid them
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+// A strict equality operator === checks the equality without type conversion
+
+console.log("2" === 2);
+
+// comparison operators return a boolean value
+// Strings are compared letter-by-letter in the “dictionary” order
+// When values of different types are compared, they get converted to numbers
+
