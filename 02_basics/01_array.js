708c0a2f27aafbb150bbb52c5576c7abeb680a89
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-11 22:29:49 +0530

    add array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+// ARRAYS:-
+// Arrays in Javascript are used to store multiple values in a single variable. They are like a list that can hold items such as numbers, strings, objects, or even other arrays.
+
+// JavaScript arrays are resizable and can contain a mix of different data types
+// JavaScript arrays are not associative arrays
+// JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on — and the last element is at the value of the array's length property minus 1.
+// JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
+
+const myArr = [0,1,2,3,4,5]
+const myHeors = ["Ironman", "Spiderman"]
+
+//const myArr = new Array(1,2,3,4)
+console.log(myArr[1]);
