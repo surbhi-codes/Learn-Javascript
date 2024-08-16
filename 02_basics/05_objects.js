4e7c84874a64486681d346ead04d81a6d891d2ac
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-16 22:31:45 +0530

    add objects

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -50,3 +50,27 @@ users[1].email
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "surbhi"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "surbhi",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
