84fed706ca6edb464e7aa1c86600a63d93630143
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-16 22:35:16 +0530

    add function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
