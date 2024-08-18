943fbc505d9a4200df51b6f6bb7eb2248e11e282
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-18 22:46:22 +0530

    functions in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,15 +1,23 @@
+// FUNCTIONS IN JS:- Block of code designed to perform a specific task. You can define a function once and then call (or use) it whenever you need to execute that task. Functions can take inputs (called parameters) and can return an output.
+
 function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
+   console.log("S");
+   console.log("U");
+   console.log("R");
+   console.log("B");
+   console.log("H");
+   console.log("I");
 }
+  sayMyName()
+/*S
+  U
+  R
+  B
+  H
+  I */
 
-// sayMyName()
-
-// function addTwoNumbers(number1, number2){
-
-//     console.log(number1 + number2);
-// }
+  function addTwoNumbers(number1, number2) // (number1, number2)- 
+  {
+      console.log(number1 + number2);   
+  }
+  addTwoNumbers()
