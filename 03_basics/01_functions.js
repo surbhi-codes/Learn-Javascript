aa70017fe79bd81dc5712ec87de552023eead65d
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-19 23:16:56 +0530

    add functins in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,4 +20,18 @@ function sayMyName(){
   {
       console.log(number1 + number2);   
   }
+  // handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
   addTwoNumbers()
