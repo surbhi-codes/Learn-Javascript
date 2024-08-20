a8e7419552b481499027bd2d7df1e79a02cf1493
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-20 22:57:40 +0530

    add function in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -51,4 +51,48 @@ function loginUserMessage(username = "Lily"){
 console.log(loginUserMessage("Surbhi")); // Surbhi just logged in
 
 
+function calculateCartPrice(num1){
+ return num1   
+}
+// console.log(calculateCartPrice(2)); // 2
+// console.log(calculateCartPrice(200, 400, 600)); // 200 
+
+function calculateCartPrice(...num1) // [... = spread or rest]
+{
+    return num1   
+   }
+   console.log(calculateCartPrice(200, 400, 600));// [ 200, 400, 600 ]
+
+   function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 20000));// [ 500, 20000 ]
+
+const user = {
+    username: "Surbhi",
+    prices:999
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);   
+} // 
+
+handleObject(user)
+handleObject(
+    {
+        username: "Lily",
+        price: 599
+    }
+)
+
+// Username is Lily and price is 599
+
+const myNewArray = [200, 800, 500, 300] // 800
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 800, 500, 300]));
 
