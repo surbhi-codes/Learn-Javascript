3e0713013a5442cad7f5f38f6d683fb34970a3c3
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-24 22:50:18 +0530

    add arrows in JS

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,38 @@
+const user = {
+    username: "surbhi",
+    price: 9999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+ user.username = "Tara"
+ user.welcomeMessage() // Tara , welcome to website
+
+ console.log(this); // {}
+
+
+
+ // chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
