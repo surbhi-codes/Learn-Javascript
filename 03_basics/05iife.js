4b059b61b8c46a12bafcd2b05ffe1d7d24adaf9b
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-28 22:59:43 +0530

    add iife in JS

--- 03_basics/05iife.js
+++ 03_basics/05iife.js
@@ -1,42 +1,11 @@
-
-// if
-const isUserloggedIn = true
-const temperature = 41
-
-// if ( temperature === 40 ){
-//     console.log("less than 50");
-// } else {
-//     console.log("temperature is greater than 50");
-// }
-
-// console.log("Execute");
-// <, >, <=, >=, ==, !=, ===, !==
-
-// const score = 200
-
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
-
-// console.log(`User power: ${power}`);
+// Immediately Invoked Function Expressions (IIFE)
 
 
-// const balance = 1000
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
 
-// if (balance > 500) console.log("test"),console.log("test2");
-
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
-    
-
-
-// } else if (balance < 900) {
-//     console.log("less than 750");
-    
-// } else {
-//     console.log("less than 1200");
-
-// }
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
