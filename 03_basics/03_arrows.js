c71f23e332aaef3af74fe0c77d3c955b059e6997
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-29 06:12:29 +0530

    add arrows in JS

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -21,6 +21,22 @@ const user = {
 
 }
 
+
+
+   // The 'this' keyword in JS refers to the object that is currently executing the code. Its value depends on where it is used:
+
+  // Global Context: In the global scope, this refers to the global object (window in browsers).
+
+// Object Method: Inside a method, this refers to the object that owns the method.
+
+//Constructor Function: When used in a constructor function, this refers to the new object being created.
+
+// Event Handler: In an event, this refers to the element that received the event.
+
+// Arrow Functions: Arrow functions don't have their own this; they inherit it from the surrounding lexical context.
+
+
+
 // user.welcomeMessage()
 //  user.username = "Tara"
 //  user.welcomeMessage() // Tara , welcome to website
@@ -46,13 +62,13 @@ const coffee = () => {
 
 coffee()
 
-// const addTwo = (num1, num2) => {
+// const addTwo = (num1, num2) => {               
 //  return num1 + num2
-// }
+// }   [ If we are using curly braces, then we have to use the return keyword ]
 
 // const addTwo = (num1, num2) => num1 + num2 //  implicit return 
 
-// const addTwo = (num1, num2) => ( num1 + num2)
+// const addTwo = (num1, num2) => ( num1 + num2)  [ If we are using parentheses, then we don't have to use the `return` keyword. ]
 
  const addTwo = (num1, num2) => ({username: "surbhi"}) // { username: 'surbhi' }
 
