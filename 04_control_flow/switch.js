a5f3216bfa3925d3a95dee3c4f9e5241a30cf3dd
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-30 07:13:32 +0530

    add switch case in JS

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,3 +1,5 @@
+// The switch statement in JavaScript is used to execute different code blocks based on the value of a variable.
+
 // switch (key) {
 //     case value:
         
@@ -7,23 +9,61 @@
 //         break;
 // }
 
-const month = "march"
+
+const month = 3
 
 switch (month) {
-    case "jan":
-        console.log("January");
-        break;
-    case "feb":
-        console.log("feb");
-        break;
-    case "march":
-        console.log("march");
-        break;
-    case "april":
-        console.log("april");
+    case 1: 
+    console.log("April");
+    break;
+
+    case 2:
+        console.log("June");
         break;
 
-    default:
+    case 3:
+        console.log("July");
+        break;
+
+    case 4:
+        console.log("August");
+        break;
+
+    case 5:
+        console.log("December");
+        break;
+    
+        default:
         console.log("default case match");
+        break;
+}
+
+
+
+const day = "Sunday";
+
+switch (day) {
+   case "Monday":
+    console.log("Monday");
+    break;
+
+   case "Sunday":
+    console.log("Sunday");
+    break;
+
+   case "Wednesday":
+    console.log("Wednesday");
+    break;
+
+   case "Saturday":
+    console.log("Saturday");
+    break;
+
+   case "Tuesday":
+    console.log("Tuesday");
+    break;
+    
+    default:
+        console.log("It's a regular day");
         break;
 }
