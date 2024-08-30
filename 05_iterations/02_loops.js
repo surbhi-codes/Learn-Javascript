5b8253afadde5b081d612174e59dc925afab9649
surbhi-codes <jhasurbhi21@gmail.com>
2024-08-30 12:56:29 +0530

    add loops in JS

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,36 @@
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+/* Output-
+Value of index is 0
+Value of index is 2
+Value of index is 4
+Value of index is 6
+Value of index is 8
+Value of index is 10
+*/
+
+let myArray = ['Tanjiro', 'Nezuko', 'Zenitsu']
+
+let arr = 0
+while (arr < myArray.length) {
+   // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+/* Output
+Value is Tanjiro
+Value is Nezuko
+Value is Zenitsu
+*/
+
+
+// let score = 1
+let score = 11 // Score is 11
+
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 10);
