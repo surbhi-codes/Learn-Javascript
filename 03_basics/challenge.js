4338ceb64221a22b91c14f3160ffa9bd5efa1c29
surbhi-codes <jhasurbhi21@gmail.com>
2024-09-08 17:15:42 +0530

    add challenge in JS

--- 03_basics/challenge.js
+++ 03_basics/challenge.js
@@ -1,15 +1,27 @@
-// 1. Make BMI Calculator-
-   
-function bmiCalculator(weight, height) {
-    var bmi = weight /  (height * height);
-    return Math.round(bmi);
+// 1. Life in weeks coding Exercise-
+
+function lifeInWeeks(age) {
+    let yearsRemaining = 90 - age;
+    let days = yearsRemaining * 365;
+    let weeks = yearsRemaining * 52;
+    let months = yearsRemaining * 12;
+
+    console.log("You have " + days + " days, " + weeks+
+" weeks, and " + months + " months left"    );
 }
 
+lifeInWeeks(21); // You have 25185 days, 3588 weeks, and 828 months left
 
-var bmi = bmiCalculator(65, 1.8);
-console.log(bmi);
+// 2.  BMI Calculator Code-
+
+function bmiCalculator(weight, height) {
+    var bmi = weight / (height * height);
+    return Math.round(bmi);  // Rounding off the result
+}
+
+// Test the function
+var bmi = bmiCalculator(65, 1.8);  // Example: 65kg weight, 1.8m height
+console.log("Your BMI is: " + bmi); // Your BMI is: 20
 
 
-// 2. Make dog age to human age formula-
 
-// const humanage = {(dogage - 2) *4}
