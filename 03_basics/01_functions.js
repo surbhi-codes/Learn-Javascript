6285bfd4f4f13dfe848ed0c0b7f0b8edd94456e2
surbhi-codes <jhasurbhi21@gmail.com>
2024-09-13 23:05:39 +0530

    add function in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -110,6 +110,5 @@ function returnSecondValue(getArray){
 console.log(returnSecondValue([200, 800, 500, 300])); // 800
 
 
-
-
-
+// Math.random() is a function that returns a floating-point number between 0 (inclusive) and 1
+//  a built-in JavaScript object that has properties and methods for mathematical constants and functions.
