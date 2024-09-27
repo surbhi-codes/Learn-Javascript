4666086a1e56a981c80de68695382a50ebd261f8
surbhi-codes <jhasurbhi21@gmail.com>
2024-09-27 16:33:43 +0530

    add Maths in JS

--- 01_basics/08_nums_&_maths.js
+++ 01_basics/08_nums_&_maths.js
@@ -16,16 +16,24 @@ const hundreds = 1000000
 
 //+++++++++++++++++++ MATHS ++++++++++++++++++++
 
+// Javascript Math Object:- The JavaScript Math object allows you to perform mathematical tasks on numbers.
+// The Math object is static
+
+
 // console.log(Math); // Object [Math] {}
 // console.log(Math.abs(-8)); // 8 to find absolute values
-// console.log(Math.round(4.6)); // 5 for roundoff
-// console.log(Math.ceil(4.2)); // 5
-// console.log(Math.floor(4.9));// 4
+
+// console.log(Math.round(4.6)); // 5 for roundoff [returns the nearest integer]
+// console.log(Math.ceil(4.2)); // 5 [returns the value of x rounded up to its nearest integer]
+// console.log(Math.floor(4.9));// 4 [returns the value of x rounded up to its nearest integer]
+
 // console.log(Math.min(9, 3, 4, 1)); // 1 [minimum number]
 // console.log(Math.max(12, 6, 8, 9)); // 12 [maximum number]
 // console.log(Math.sqrt(5)); // 2.23606797749979
+// console.log(Math.PI)       // returns PI
 
-console.log(Math.random()); // [0 to 1]
+
+console.log(Math.random()); // [returns a random decimal number between 0 (inclusive) and 1 (exclusive)]
 console.log((Math.random()*10) + 1); // 6.890077461822239 
 console.log(Math.floor(Math.random()*10) + 1); // [1 to 9]
 
