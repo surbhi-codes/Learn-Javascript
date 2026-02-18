b96a614c5b59e3fc861ece73b4d5a97416846bd0
surbhi-codes <jhasurbhi21@gmail.com>
2026-02-18 20:27:57 +0530

    add switch

--- 01_basics/4_switch/index.js
+++ 01_basics/4_switch/index.js
@@ -1,30 +1,31 @@
 // SWITCH = used when we have multiple possible conditoins for one variable
 // cleaner than using many else if statements
 
-let day = "7";
+let testScore = 80;
+let letterGrade;
 
-switch(day) {
-    case 1:
-        console.log("It's Monday");
-        break;
-    case 2:
-        console.log("It's Tuesday");
-        break;
-    case 3:
-        console.log("It's Wednesday");
-        break;
-    case 4:
-        console.log("It's Thursday");
-        break;
-    case 5:
-        console.log("It's Friday");
-        break;
-    case 6:
-        console.log("It's Saturday");
-        break;
-    case 7:
-        console.log("It's Sunday");
-        break;
+switch(true){
+    case testScore >= 90:
+    letterGrade = "A";
+    break;
+
+    case testScore >= 80:
+    letterGrade = "B";
+    break;
+
+    case testScore >= 70:
+    letterGrade = "C";
+    break;
+
+    case testScore >= 60:
+    letterGrade = "D";
+    break;
+    
+    case testScore >= 50:
+    letterGrade = "E";
+    break;
+
     default:
-      console.log(`${day} is not a day`);
+        letterGrade = "F";
 }
+console.log(letterGrade);
