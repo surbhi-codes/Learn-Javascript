404c51b1e72b794bcd61df3010ffba72300dbc8d
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-09 08:39:56 +0530

    add operators

---
+++ 01_basics/8_assignment_operator/index.js
@@ -0,0 +1,47 @@
+// = assignment operator
+// == comparison operator
+// === strict equality operator (compare if values & datatype are equal)
+// != inequality operator
+// !== strict inequality operator
+
+// const PI = 3.14;
+
+// if (PI === "3.14"){
+//     console.log("That is PI");
+// }
+// else {
+//     console.log("That is NOT PI");
+// }
+// output - That is NOT PI
+// 3.14 → number
+// "3.14"→ string
+// Since the types are different, the condition is false.
+
+// const PI = 3.14;
+
+// if (PI === 3.14){
+//     console.log("That is PI");
+// }
+// else {
+//     console.log("That is NOT PI");
+// }
+// output - That is PI
+
+// const PI = 3.14;
+
+// if (PI != "3.14"){
+//     console.log("That is NOT PI");
+// }
+// else {
+//     console.log("That is PI");
+// }
+// output - That is PI
+
+const PI = "3.14";
+
+if (PI !== "3.14"){
+    console.log("That is NOT PI");
+}
+else {
+    console.log("That is PI");
+}
