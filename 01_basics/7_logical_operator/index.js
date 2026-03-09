b40c2bc22f28b294ddc6eac1230c4a9950bd5c1f
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-09 08:16:29 +0530

    add logical operator

---
+++ 01_basics/7_logical_operator/index.js
@@ -0,0 +1,31 @@
+// logical operators = used to combine or manipulate boolean values (true or false)
+// AND = &&
+// OR = ||
+// NOT = !
+
+// const temp = 45;
+
+// if (temp > 0 && temp <= 30 ) {
+//     console.log("The weather is GOOD");
+// }
+// else {
+//     console.log("The weather is BAD");
+// }
+
+// const temp = 300;
+
+// if (temp <= 0 || temp > 30){
+//     console.log("The weather is BAD");
+// }
+// else {
+//     console.log("The weather is GOOD");
+// }
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It's CLOUDY");
+}
+else {
+    console.log("It's SUNNY");
+}
