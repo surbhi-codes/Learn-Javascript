706b02077967e97eeca9fabc662dddb984e4e4db
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-24 08:46:16 +0530

    add for loop

--- 01_basics/9_loops/For/index.js
+++ 01_basics/9_loops/For/index.js
@@ -1,5 +1,11 @@
 // for loop - repeat some code a LIMITED amount of times
 
-for (let i = 0; i < 10; i++){
-    console.log(i);
+for (let i = 1; i <= 20; i++){
+
+    if(i == 13){
+        break;
+    }
+    else {
+        console.log(i);
+    }
 }
