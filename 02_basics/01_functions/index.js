a3b2c5ca6b557f020e225c5c1d851077581f649c
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-26 22:48:29 +0530

    Add function

---
+++ 02_basics/01_functions/index.js
@@ -0,0 +1,16 @@
+// Function - A section of reusable code.
+//            Declare code once, use it whenever you want.
+//            Call the function to execute that code.
+
+function happyBirthday(username, age){
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log(`Happy birthday dear ${username}!`);
+    console.log("Happy birthday to you!");
+    console.log(`You're ${age} years old`);
+}
+
+happyBirthday("Surbhi", 22);
+happyBirthday("Zein", 23);
+happyBirthday("Mia", 24);
+
